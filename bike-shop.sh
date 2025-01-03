09169090fbaa56aaf5b230edddc9bc323bf64784
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-01-03 17:29:17 -0300

    Modifico script para reconstruir hasta el 50% del ejercicio

--- bike-shop.sh
+++ bike-shop.sh
@@ -24,11 +24,44 @@ MAIN_MENU() {
 
 RENT_MENU() {
   # get available bikes
+  AVAILABLE_BIKES=$($PSQL "SELECT bike_id,type,size FROM bikes WHERE available='true' ORDER BY bike_id")
 
   # if no bikes available
+  if [[ -z $AVAILABLE_BIKES ]]
+  then
+    # send to main menu
+    MAIN_MENU "Sorry, we don't have any bikes available right now." 
+    else
+    
+    #display available bikes
+    echo -e "\nHere are the bikes we have available:"
+    echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE
+    do
+      echo "$BIKE_ID) $SIZE\" $TYPE Bike" 
+    done
 
-  # send to main menu
-
+    #ask for bike to rent
+    echo -e "\nWhich one would you like to rent?"
+    read BIKE_ID_TO_RENT
+    
+    #if input is not a number
+    if [[ ! $BIKE_ID_TO_RENT =~ ^[0-9]+$ ]]
+    then
+      #send to main menu
+      MAIN_MENU "That is not a valid bike number."
+    else
+      
+      #get bike availability
+      BIKE_AVAILABILITY=$($PSQL "SELECT available FROM bikes WHERE bike_id = $BIKE_ID_TO_RENT and available = true")
+      
+      #if not available    
+      if [[ -z $BIKE_AVAILABILITY ]]
+      then
+        #send to main menu
+        MAIN_MENU "That bike is not available."
+      fi    
+    fi    
+  fi
 }
 
 RETURN_MENU() {
